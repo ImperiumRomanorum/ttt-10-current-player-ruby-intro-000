d57d62d150f92c5e5d95fc77e7ab897784fc996e
Michael Calvelo <agedtoastlet@gmail.com>
2019-09-15 03:50:16 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,11 +8,12 @@ def turn_count(board)
   return counter
 end
 
-def current_player(board)
-  if turn_count(board) % 2 == 0
-    "X"
-  else
-    "O"
-  end  
-end  
+#def current_player(board)
+#  if turn_count(board) % 2 == 0
+#    "X"
+#  else
+#    "O"
+#  end  
+#end  
 
+curren_player(board) turn_count(board) % 2 == 0 ? "X" : "O"
